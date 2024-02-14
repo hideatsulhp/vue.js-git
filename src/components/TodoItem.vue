b68547fcf41c1e2f0631092eedd01312a935b8a4
hideatsulhp <lhp.pa.jp@gmail.com>
2024-02-14 10:06:47 +0900

    課題提出

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-col :span="12">
+      <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+        <el-row :gutter="12">
+          <el-col :span="21">{{ todo }}</el-col>
+          <el-col :span="3">
+            <el-button @click="removeTodo(index)" type="success" icon="el-icon-check" circle></el-button>
+          </el-col>
+        </el-row>
+      </el-card>
+    </el-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: String,
+    index: Number, // index プロパティを追加
+  },
+  methods: {
+    removeTodo() {
+      // 親コンポーネントにイベントを発火
+      this.$emit('remove-todo' , this.index);
+    }
+  }
+
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
+  devServer: {
+    hot: true, // HMRを有効にする
+    liveReload: true, // ページの自動リロード
+  },
   transpileDependencies: true
 })
